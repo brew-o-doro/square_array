25b0fee4d3f9eae365be1b4921727c6499d572e0
Teodoro <tedtke514@gmail.com>
2022-09-24 19:46:44 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def square_array(array)
   # your code here
+    new_numbers = []
+    array.each{ |x| new_numbers << x ** 2}
+    return new_numbers
+    
+    new_array = array.collect{|x| x ** 2}
+    #binding.pry
+    return new_array
 end
